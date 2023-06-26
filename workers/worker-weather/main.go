f64f5cf4b2b48f46189e300326d6e556574bcd96
Lisandro Torresetti <lisandro.ltorresetti@mercadolibre.com>
2023-06-26 15:19:19 -0300

    Refactor weather worker: add constants, use functions from internal package, and other changes

--- workers/worker-weather/main.go
+++ workers/worker-weather/main.go
@@ -1,87 +1,74 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	common "github.com/Ignaciocl/tp1SisdisCommons"
+	commonDtos "github.com/Ignaciocl/tp1SisdisCommons/dtos"
 	commonHealthcheck "github.com/Ignaciocl/tp1SisdisCommons/healthcheck"
 	"github.com/Ignaciocl/tp1SisdisCommons/queue"
 	"github.com/Ignaciocl/tp1SisdisCommons/utils"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"os"
 	"strconv"
-	"strings"
-	"time"
+	"worker-weather/internal/dtos"
+	weatherErrors "worker-weather/internal/errors"
+	"worker-weather/internal/filter"
+	"worker-weather/internal/transformer"
 )
 
 const (
-	serviceName = "worker-weather"
-	dateLayout  = "2006-01-02"
+	logLevelEnvVar   = "LOG_LEVEL"
+	defaultLogLevel  = "DEBUG"
+	serviceName      = "worker-weather"
+	dateLayout       = "2006-01-02"
+	connectionString = "rabbit"
 )
 
-func getDate(date string) string {
-	d, err := time.Parse(dateLayout, date)
+// InitLogger Receives the log level to be set in logrus as a string. This method
+// parses the string and set the level to the logger. If the level string is not
+// valid an error is returned
+func InitLogger(logLevel string) error {
+	level, err := log.ParseLevel(logLevel)
 	if err != nil {
-		utils.FailOnError(err, "Invalid date while parsing")
+		return err
 	}
-	return strings.Split(d.AddDate(0, 0, -1).String(), " ")[0]
-}
 
-type Weather struct {
-	Date string  `json:"date"`
-	Prec float64 `json:"prectot,string"`
-}
-
-type WorkerWeather struct {
-	City string  `json:"city"`
-	Data Weather `json:"data,omitempty"`
-	Key  string  `json:"key"`
-	common.EofData
-}
-
-type SendableDataWeather struct {
-	Date string  `json:"date"`
-	Prec float64 `json:"prec"`
-}
-
-type JoinerData struct {
-	City string              `json:"city"`
-	Data SendableDataWeather `json:"weatherData"`
-	Key  string              `json:"key"`
-	EOF  bool                `json:"EOF"`
-}
-
-func processData(
-	weather WorkerWeather,
-	qt queue.Sender[JoinerData]) {
-	if weather.Data.Prec < 30 {
-		return
+	customFormatter := &log.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   false,
 	}
-	d := JoinerData{
-		City: weather.City,
-		Key:  weather.Key,
-		EOF:  weather.EOF,
-		Data: SendableDataWeather{
-			Date: getDate(weather.Data.Date),
-		},
-	}
-	err := qt.SendMessage(d, "")
-	if err != nil {
-		utils.FailOnError(err, "Couldn't send message to joiner stations, failing horribly")
-	}
+	log.SetFormatter(customFormatter)
+	log.SetLevel(level)
+	return nil
 }
 
 func main() {
+	logLevel := os.Getenv(logLevelEnvVar)
+	if logLevel == "" {
+		logLevel = defaultLogLevel
+	}
+
+	if err := InitLogger(logLevel); err != nil {
+		panic(fmt.Sprintf("error initializing logger: %v", err))
+	}
+
 	id := os.Getenv("id")
+	if id == "" {
+		panic("missing weather worker id")
+	}
+
 	distributors, err := strconv.Atoi(os.Getenv("distributors"))
 	utils.FailOnError(err, "missing env value of distributors")
-	inputQueue, _ := queue.InitializeReceiver[WorkerWeather]("weatherWorkers", "rabbit", id, "", nil)
-	outputQueueWeather, _ := queue.InitializeSender[JoinerData]("weatherQueue", 0, inputQueue, "")
+	inputQueue, _ := queue.InitializeReceiver[dtos.InputData]("weatherWorkers", connectionString, id, "", nil)
+	outputQueueWeather, _ := queue.InitializeSender[dtos.JoinerData]("weatherQueue", 0, inputQueue, "")
 	v := make([]common.NextToNotify, 1)
 	v = append(v, common.NextToNotify{
 		Name:       "weatherQueue",
 		Connection: outputQueueWeather,
 	})
 	iqEOF, _ := common.CreateConsumerEOF(v, "weatherWorkers", inputQueue, distributors)
-	grace, _ := common.CreateGracefulManager("rabbit")
+	grace, _ := common.CreateGracefulManager(connectionString)
 	defer grace.Close()
 	defer common.RecoverFromPanic(grace, "")
 	defer iqEOF.Close()
@@ -93,24 +80,76 @@ func main() {
 			data, msgId, err := inputQueue.ReceiveMessage()
 			if err != nil {
 				utils.FailOnError(err, "Failed while receiving message")
-				continue
 			}
-			if data.EOF {
-				iqEOF.AnswerEofOk(data.IdempotencyKey, nil)
+
+			metadata := data.Metadata
+
+			if metadata.IsEOF() {
+				// ToDo: nacho que va aca?
+				iqEOF.AnswerEofOk(metadata.GetIdempotencyKey(), nil)
 				utils.LogError(inputQueue.AckMessage(msgId), "failed while trying ack")
 				continue
 			}
-			processData(data, outputQueueWeather)
-			inputQueue.AckMessage(msgId)
+
+			// Now we have a batch with weather data
+			for _, rawWeatherData := range data.Data {
+				// 1. Transform the raw data into WeatherData
+				weatherData, err := transformer.Transform(rawWeatherData)
+				if err != nil {
+					if errors.Is(err, weatherErrors.ErrInvalidWeatherData) {
+						log.Debug("Invalid weather data")
+						continue
+					}
+					log.Errorf("Error transforming raw weather data: %v", err)
+					panic(err)
+				}
+
+				// 2. Get extra data contain in the message
+				rawMessageExtraData := utils.GetMetadataFromMessage(rawWeatherData)
+				metadata.IdempotencyKey = fmt.Sprintf( //clientID-batchNum-messageNum-city
+					"%s-%s-%s-%s",
+					rawMessageExtraData.ClientID,
+					rawMessageExtraData.BatchNumber,
+					rawMessageExtraData.MessageNumber,
+					rawMessageExtraData.City,
+				)
+
+				// 3. Check if the Weather Data is valid to go to the next stage
+				if filter.IsValid(*weatherData) {
+					sendData(rawMessageExtraData.ClientID, *weatherData, metadata, outputQueueWeather)
+					continue
+				}
+				log.Debugf("Filtering weather data: %+v", *weatherData)
+			}
+
+			err = inputQueue.AckMessage(msgId)
+			if err != nil {
+				utils.FailOnError(err, "error ACK weather data")
+			}
 		}
 	}()
 
 	healthCheckerReplier := commonHealthcheck.InitHealthCheckerReplier(serviceName + id)
 	go func() {
 		err := healthCheckerReplier.Run()
-		log.Printf("healtchecker error: %v", err)
+		log.Errorf("healtchecker error: %v", err)
 	}()
 
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	log.Info(" [*] Waiting for messages. To exit press CTRL+C")
 	common.WaitForSigterm(grace)
+}
+
+// sendData sends weather data to the next stage through the given sender
+func sendData(clientID string, weatherData dtos.WeatherData, metadata commonDtos.Metadata, sender queue.Sender[dtos.JoinerData]) {
+	dataToSend := dtos.JoinerData{
+		City:           metadata.GetCity(),
+		IdempotencyKey: metadata.GetIdempotencyKey(),
+		EOF:            metadata.IsEOF(),
+		Data:           weatherData,
+	}
+
+	err := sender.SendMessage(dataToSend, clientID)
+	if err != nil {
+		utils.FailOnError(err, "Couldn't send message to weather joiner, failing horribly")
+	}
 }
